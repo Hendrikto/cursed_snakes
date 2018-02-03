9be771c0caf77c6967a58639cd272abdf9ba6702
Hendrikto <hendrik.to@gmail.com>
2018-02-03 02:42:51 +0100

    Initialize and draw a snake

--- cursed_snakes.py
+++ cursed_snakes.py
@@ -2,6 +2,8 @@
 
 import curses
 
+from model.snake import Snake
+
 __author__ = "Hendrik Werner"
 
 board_width = 50
@@ -20,16 +22,20 @@ def add_titleborder(window, title):
 
 
 def draw_loop(board):
+    snake = Snake(1, 1)
     curses.curs_set(0)
     board.resize(board_height, board_width)
     add_titleborder(board, "Game Board")
     score = curses.newwin(board_height, score_width, 0, board_width + 1)
     add_titleborder(score, "Score")
-    board.refresh()
-    score.refresh()
     key = None
     while key != 'q':
         key = board.getkey()
+        snake.update_head()
+        snake.draw(board)
+        snake.update_tail()
+        board.refresh()
+        score.refresh()
 
 if __name__ == '__main__':
     curses.wrapper(draw_loop)
